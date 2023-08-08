1208f9bdcaefa51529c9638db4e5f78d5c89e2fa
khaizr0 <caophankhai123@gmail.com>
2023-08-08 20:08:05 +0700

    update

---
+++ Js/product.js
@@ -0,0 +1,15 @@
+// select all thumbnails
+const galleryThumbnail = document.querySelectorAll(".thumbnails-list li");
+// select featured
+const galleryFeatured = document.querySelector(".product-gallery-featured img");
+
+// loop all items
+galleryThumbnail.forEach((item) => {
+  item.addEventListener("mouseover", function () {
+    let image = item.children[0].src;
+    galleryFeatured.src = image;
+  });
+});
+
+// show popover
+$(".main-questions").popover('show');
